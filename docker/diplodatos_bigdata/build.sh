58d40241ae5ed17c22a7e7daedaa10cfcad99aa7
Damian Barsotti <damian@famaf.unc.edu.ar>
2021-10-13 10:44:28 -0300

    Files from private

--- docker/diplodatos_bigdata/build.sh
+++ docker/diplodatos_bigdata/build.sh
@@ -9,6 +9,26 @@ IM=diplodatos/bigdata:$IM_VERSION
 
 IM_BASE=apache/zeppelin:$IM_BASE_VERSION
 
+echoerr() { echo "$@" 1>&2; }
+
+prog_exists() {
+    if ! command -v $1 &> /dev/null
+    then
+        echoerr $1 cannot be found
+        exit 1
+    fi
+}
+
+for prg in realpath docker grep
+do
+    prog_exists $prg
+done
+
+DIRNAME="$(dirname "$0")"
+GIT_DIR="$(realpath "$DIRNAME")"/../..
+
+DOCKER_GIT_DIR="$GIT_DIR"/docker
+
 # Stop all containers
 for c in $(docker ps -a -q  --filter ancestor=$IM)
 do
