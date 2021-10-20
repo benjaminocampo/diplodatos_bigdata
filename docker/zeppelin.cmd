003d2bd536e3b7180a04fe8cee71934b286289fd
Damian Barsotti <damian@famaf.unc.edu.ar>
2021-10-20 18:00:42 -0300

    Files from private

--- docker/zeppelin.cmd
+++ docker/zeppelin.cmd
@@ -13,5 +13,6 @@ docker run -u %UID% -it --rm --hostname localhost -p 8080:8080 -p 4040:4040^
 	-v "%DIRNAME%vols\logs:/opt/zeppelin/logs"^
 	-v "%DIRNAME%vols\notebook:/notebook"^
 	-v "%GITDIR%:/diplodatos_bigdata"^
-	-e ZEPPELIN_LOG_DIR=/logs -e ZEPPELIN_NOTEBOOK_DIR=/notebook^
+	-e ZEPPELIN_LOG_DIR=/opt/zeppelin/logs -e ZEPPELIN_NOTEBOOK_DIR=/notebook^
+	-e ZEPPELIN_INTERPRETER_CONNECT_TIMEOUT=120000^
 	--name diplodatos_bigdata %IM%
